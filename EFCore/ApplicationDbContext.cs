576045e417b4ca99aa3bb0d97bf6fe70ac71dee7
ahmedsalehalzabr <ahmedalzabr1@gmail.com>
2024-01-04 19:08:05 +0500

    Add project files.

---
+++ EFCore/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    internal class ApplicationDbContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=EFECore;Integrated Security=True");
+        public DbSet<Eemployeee> Employees { get; set; }
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCore;
+
+var _context = new ApplicationDbContext();
+var employee = new Eemployeee
+{
+    Name = "Employee 1"
+};
+_context.Employees.Add(employee);
+_context.SaveChanges();
